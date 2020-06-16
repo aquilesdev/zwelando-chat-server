8db48d4c847730157d3ed1308d587a843f3d9b10
Antonio da Paixao <aquilesdapaixao@gmail.com>
2020-06-16 09:28:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zwelando-chat-server
---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const dotenv = require('dotenv');
+
+dotenv.config({path:'.env'});
+
+
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(router);
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+
+    console.log('A new user join the conversation');
+
+    socket.on('disconnect', () => {
+        console.log('An user left the conversation');
+    });
+});
+
+server.listen(PORT, console.log(`Server is running on port ${PORT}`));
